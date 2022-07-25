8ac825c0034209096e759b335323b69e191453a3
michaelmilanez <aceleradorapp@gmail.com>
2022-07-25 12:16:31 -0300

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,11 +6,11 @@
     <div class="content-tools">
         <div class="container">            
             
-            <div class="row justify-content-md-center">
-                <div class="col-md-10">
-                    
-                </div>
-            </div>             
+        <div class="row justify-content-md-center">
+            <div class="col-md-10">
+                <p style="font-size:20px">Olá, Sua conta ainda é de <b>visitante</b>, por este motivo não tem permissões para acessar algumas funcionalidades do site ou fazer download das ferramentas. Assim que sua conta for verificada e validada receberá um aviso em sua conta de e-mail com a liberação de acesso.</p>
+            </div>
+        </div>            
 
         </div>
     </div>
